9d47910adb2bd01235809ac18f6f2181396f290f
Martin Dilling-Hansen <martindilling@gmail.com>
2011-12-14 01:52:58 +0100

    Added login page template
    Small updates in Bootstrap-Extend

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Admin - Login</title>
+	
+	<!-- HTML5 shim, for IE6-8 support of HTML elements -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+		
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.css" />
+	<link rel="stylesheet" href="css/bootstrap-extend/bootstrap-extend.css" />
+	<link rel="stylesheet" href="css/style.css" />
+        
+	<script src="js/jquery-1.7.1.js"></script>
+<!--	<script src="js/bootstrap/bootstrap-modal.js"></script>-->
+<!--	<script src="js/bootstrap/bootstrap-alerts.js"></script>-->
+<!--	<script src="js/bootstrap/bootstrap-twipsy.js"></script>-->
+<!--	<script src="js/bootstrap/bootstrap-popover.js"></script>-->
+<!--	<script src="js/bootstrap/bootstrap-dropdown.js"></script>-->
+<!--	<script src="js/bootstrap/bootstrap-scrollspy.js"></script>-->
+<!--	<script src="js/bootstrap/bootstrap-tabs.js"></script>-->
+<!--	<script src="js/bootstrap/bootstrap-buttons.js"></script>-->
+
+	<script src="js/bootstrap-extend/contentlist.js"></script>
+</head>
+<body>
+
+<!-- Login box
+================================================== -->
+	<div class="container ">
+		<table class="bordered-table zebra-striped" style="margin-top: 40px">
+			<thead>
+				<tr>
+					<th>Link</th>
+					<th>Side</th>
+					<th>Beskrivelse</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td width="60"><a href="login.php" class="btn">Goto</a></td>
+					<td>Login</td>
+					<td>Login til adminpanelet</td>
+				</tr>
+				<tr>
+					<td><a href="#" class="btn danger">Goto</a></td>
+					<td>Adminpanel</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td><a href="#" class="btn danger">Goto</a></td>
+					<td>Event</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td><a href="#" class="btn danger">Goto</a></td>
+					<td>Nyt event</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td><a href="#" class="btn danger">Goto</a></td>
+					<td>Sponsorer</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td><a href="#" class="btn danger">Goto</a></td>
+					<td>Ny sponsor</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td><a href="#" class="btn danger">Goto</a></td>
+					<td>Donationer</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td><a href="#" class="btn danger">Goto</a></td>
+					<td>Ny donation</td>
+					<td>&nbsp;</td>
+				</tr>
+			</tbody>
+		</table>
+	</div><!-- /container -->
+
+</body>
+
+</html>
